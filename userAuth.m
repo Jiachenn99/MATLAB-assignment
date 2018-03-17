e12c6ac9f57eade5cb89b980aa9ab414f316f692
unknown <jiachenn99@gmail.com>
2018-03-17 23:09:20 +0800

    opening file and closing file

--- userAuth.m
+++ userAuth.m
@@ -2,5 +2,22 @@
 function string = userAuth(x)
     %Requesting user to input their username
     string = menu('Are you a new or existing user?', 'New' , 'Existing')
+    
+    switch(string)
+        case 1 %When username exists
+            username = input('Please enter your username: ')
+            
+            
+        case 2 %When username does not exist
+            username = input('Please enter your username: ')
+            fileID = fopen('highscores.txt' , 'w');
+            formatSpec = '%s';
+            %insert ismember function here for username
+            
+            
+            
+    
+    
+    
 end
 %Receives a string x to check whether the user's name exist
